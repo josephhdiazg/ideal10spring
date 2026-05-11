17a586a06cd09eef40041eeed2e61c510bd1e856
Joseph Diaz <194326262+josephhdiazg@users.noreply.github.com>
2026-05-11 10:22:48 -0500

    chore: first commit

---
+++ src/main/java/com/ideal_10/ideal10spring/Ideal10springApplication.java
@@ -0,0 +1,13 @@
+package com.ideal_10.ideal10spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ideal10springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ideal10springApplication.class, args);
+	}
+
+}
