4ab66f5cce25630cfb86429142df928336db2ef0
Joseph Diaz <194326262+josephhdiazg@users.noreply.github.com>
2026-05-28 20:43:06 -0500

    feat(config): add JWT configuration to environment files and update README

--- src/test/java/com/ideal_10/ideal10spring/Ideal10springApplicationTests.java
+++ src/test/java/com/ideal_10/ideal10spring/Ideal10springApplicationTests.java
@@ -1,12 +1,15 @@
 package com.ideal_10.ideal10spring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class Ideal10springApplicationTests {
 
 	@Test
-	void applicationClassExists() {
-		new Ideal10springApplication();
+	void contextLoads() {
 	}
 
 }
